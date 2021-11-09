c4e07a39e8e5bea27d241cc2fc075e68de9b4e92
witzkvn <witz.kevin05@gmail.com>
2021-11-09 21:10:56 +0100

    recuperer potion depuis les cookies

--- PotionsMaker/Controllers/HomeController.cs
+++ PotionsMaker/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using PotionsMaker.Tools;
 
 namespace PotionsMaker.Controllers
 {
@@ -14,7 +15,7 @@ namespace PotionsMaker.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
-
+        Potion currentPotion;
 
 
         public HomeController(ILogger<HomeController> logger)
@@ -30,7 +31,55 @@ namespace PotionsMaker.Controllers
                 nosIngredients = db.Ingredients.ToList();
             }
             ViewData["Ingredients"] = nosIngredients;
+            currentPotion = this.GetPotionFromCookies();
+            currentPotion.Ingredients.Add(new Ingredient()
+            {
+                Nom = "test",
+                IngredientId = 1,
+            });
+            SavePotionIntoCookies(currentPotion);
+            Dictionary<Ingredient, int> listeIngCurrentPotion = ParsePotionIngredients();
+            ViewBag.potion = currentPotion;
+            ViewBag.listeIngredients = listeIngCurrentPotion;
             return View();
+        }
+
+        private Dictionary<Ingredient, int> ParsePotionIngredients()
+        {
+            Dictionary<Ingredient, int> listeIng = new Dictionary<Ingredient, int>();
+            foreach (var ing in currentPotion.Ingredients)
+            {
+                if (!listeIng.ContainsKey(ing))
+                {
+                    listeIng.Add(ing, 1);
+                }
+                else
+                {
+                    listeIng[ing] = listeIng[ing] + 1;
+                }
+            }
+
+            return listeIng;
+        }
+
+        public Potion GetPotionFromCookies()
+        {
+            Potion currentPotion;
+            if (HttpContext.Session.GetObjectFromJson<Potion>("currentPotion") == null)
+            {
+                currentPotion = new Potion();
+                HttpContext.Session.SetObjectAsJson("currentPotion", currentPotion);
+            }
+            else
+            {
+                currentPotion = HttpContext.Session.GetObjectFromJson<Potion>("currentPotion");
+            }
+            return currentPotion;
+        }
+
+        public void SavePotionIntoCookies(Potion potion)
+        {
+            HttpContext.Session.SetObjectAsJson("currentPotion", potion);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
