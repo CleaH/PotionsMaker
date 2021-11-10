b1d5171e2eb6e20206671543bf39c70f2ff12b3a
CleaH <clea.he@gmail.com>
2021-11-10 14:23:00 +0100

    ajout images

--- PotionsMaker/Models/Potion.cs
+++ PotionsMaker/Models/Potion.cs
@@ -6,21 +6,20 @@ namespace PotionsMaker.Models
     {
         public string ActualColor { get; set; }
         public List<Ingredient> Ingredients { get; set; }
-        public Dictionary<string, (int?, string)> Stats { get; set; }
-        public int compteur;
+        public Dictionary<string, int?> Stats { get; set; }
 
         public Potion()
         {
             Ingredients = new List<Ingredient>();
             ActualColor = "#FFFFFF";
-            Stats = new Dictionary<string, (int?, string)> {
-                {"Toxicité", (0, "")},
-                {"Soin", (0, "")},
-                {"Amour", (0, "")},
-                {"Puissance", (0, "")},
-                {"Mana", (0, "")},
-                {"Intelligence", (0, "")},
-                {"Agilité", (0, "")}
+            Stats = new Dictionary<string, int?> {
+                {"Toxicité", 0},
+                {"Soin", 0},
+                {"Amour", 0},
+                {"Puissance", 0},
+                {"Mana", 0},
+                {"Intelligence", 0},
+                {"Agilité", 0}
             };
         }
     }
