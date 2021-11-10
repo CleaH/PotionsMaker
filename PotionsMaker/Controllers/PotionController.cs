eab1b7d5b55d9af4a772dc32823829eb094af7a7
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-10 11:11:08 +0100

    Adaptation de la methode pour aller avec le tuple

--- PotionsMaker/Controllers/PotionController.cs
+++ PotionsMaker/Controllers/PotionController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using PotionsMaker.Models;
@@ -10,20 +11,32 @@ namespace PotionsMaker
         {
             foreach (Ingredient ingredient in potion.Ingredients)
             {
-                potion.Stats["Toxicité"] += ingredient.Toxicity;
-                potion.Stats["Soin"] += ingredient.Soin;
-                potion.Stats["Amour"] += ingredient.Amour;
-                potion.Stats["Puissance"] += ingredient.Puissance;
-                potion.Stats["Mana"] += ingredient.Mana;
-                potion.Stats["Intelligence"] += ingredient.Intelligence;
-                potion.Stats["Agilité"] += ingredient.Agilite;
+
+                potion.Stats["Toxicité"] = (potion.Stats["Toxicité"].Item1 + ingredient.Toxicity, potion.Stats["Toxicité"].Item2);
+                potion.Stats["Soin"] = (potion.Stats["Soin"].Item1 + ingredient.Soin, potion.Stats["Soin"].Item2);
+                potion.Stats["Amour"] = (potion.Stats["Amour"].Item1 + ingredient.Amour, potion.Stats["Amour"].Item2);
+                potion.Stats["Puissance"] = (potion.Stats["Puissance"].Item1 + ingredient.Puissance, potion.Stats["Puissance"].Item2);
+                potion.Stats["Mana"] = (potion.Stats["Mana"].Item1 + ingredient.Mana, potion.Stats["Mana"].Item2);
+                potion.Stats["Intelligence"] = (potion.Stats["Intelligence"].Item1 + ingredient.Intelligence, potion.Stats["Intelligence"].Item2);
+                potion.Stats["Agilité"] = (potion.Stats["Agilité"].Item1 + ingredient.Agilite, potion.Stats["Agilité"].Item2);
             }
         }
-        static public Dictionary<string, int?> GetMaxStats(Potion potion)
+        static public Dictionary<string, (int?, string)> GetMaxStats(Potion potion)
         {
             SetStats(potion);
-            Dictionary<string, int?> sortedStats = potion.Stats.OrderByDescending( s => s.Value).ToDictionary(p => p.Key, p => p.Value);
+            Dictionary<string, (int?, string)> sortedStats = potion.Stats.OrderByDescending( s => s.Value.Item1).ToDictionary(p => p.Key, p => p.Value);
             return sortedStats;
         }
+        static public void GetMaxEffects(Potion potion) 
+        {
+            Dictionary<string, (int?, string)> sortedStats = GetMaxStats(potion);
+            switch (sortedStats["Toxicité"].Item1)
+            {
+                
+
+            }
+        }
+            
+        
     }
 }
